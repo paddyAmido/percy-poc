cc5f3464a82a911d2ffab047861a14b596f885f7
Patrick Hendron <patrick.hendron@amido.com>
2022-05-10 16:07:59 +0100

    update test

--- cypress/integration/visualTest.spec.js
+++ cypress/integration/visualTest.spec.js
@@ -2,7 +2,7 @@ import '@percy/cypress';
 
 describe('Does my UI match', () => {
     it('what does google look like today', () => {
-      cy.visit('https://www.yahoo.com');
+      cy.visit('https://www.google.com');
       expect(true).to.equal(true)
       // Take a snapshot for visual diffing
       cy.percySnapshot('google snap shot');
