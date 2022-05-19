5877ea52ad9ccc4a3c23cb50bb6043ba47ad6ca2
Patrick Hendron <patrick.hendron@amido.com>
2022-05-19 15:30:35 +0100

    uploading check

--- cypress/integration/visualTest.spec.js
+++ cypress/integration/visualTest.spec.js
@@ -2,9 +2,16 @@ import '@percy/cypress';
 
 describe('Does my UI match', () => {
     it('what does google look like today', () => {
-      cy.visit('https://www.google.com');
+      cy.visit('https://www.yahoo.com');
       expect(true).to.equal(true)
       // Take a snapshot for visual diffing
       cy.percySnapshot('google snap shot');
+    })
+
+    it ('what does find your gym look like', () => {
+      cy.visit('https://www.thegymgroup.com/join-now/find-your-gym/');
+      expect(true).to.equal(true)
+
+      cy.percySnapshot('Find your gym');
     })
   })
